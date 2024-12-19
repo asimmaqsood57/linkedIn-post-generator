e296f09d4bbca9f9f9d14feb5863b7178c4ee243
asimmaqsood57 <asimmaqsood57@gmail.com>
2024-12-19 21:02:07 +0500

    Project initialzied

---
+++ src/app/components/GeneratePost/index.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const GeneratePost = () => {
+  return <div className="text-center">GeneratePost</div>;
+};
+
+export default GeneratePost;
---
+++ src/app/generate-post/page.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+// components
+import GeneratePost from "../components/GeneratePost";
+
+const GeneratePostPage = () => {
+  return (
+    <div>
+      <GeneratePost />
+    </div>
+  );
+};
+
+export default GeneratePostPage;
